e5b9300dd4c0e7ead4d8969fa0ffaf9076b958d3
Tysen Bair <tysenbair@mail.weber.edu>
2017-02-14 16:39:37 +0000

    initail commit

---
+++ Tau_Hw5_task2.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Tau_Hw5_task2.c
+ *       	Usage:  ./Tau_Hw5_task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:37:43 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Tysen Bair (), tysenbair@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
