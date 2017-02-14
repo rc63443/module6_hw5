4775de7e78dcdbe9cb50b901204126fded816d4f
Robert Christensen <robertchristensen1@mail.weber.edu>
2017-02-14 16:38:55 +0000

    First time commiting

---
+++ tau_hw5_task1.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  tau_hw5_task1.c
+ *       	Usage:  ./tau_hw5_task1.c
+ *    Description:  Homework 5 task 1
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:37:45 PM
+ *       Compiler:  gcc -Wall -Werr
+ *         Author:  Robert Christensen (), robertchristensen1@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
