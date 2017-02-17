caac064e1144a17d28f24c285ed5bb5921705231
Robert Christensen <robertchristensen1@mail.weber.edu>
2017-02-17 00:51:54 +0000

    Almost done.

--- tau_hw5_task1.c
+++ tau_hw5_task1.c
@@ -14,15 +14,33 @@
 #include <stdio.h>		/* For Standard I/O */
 
 /* Function Prototypes */
+void AnnulusMoment(float a, float b)
 
 /* Main Program */
 int main(void)
 {
+	float r1;
+	float r2;
+	float second;
+
+	printf("Please enter a value for outer radius (r2):\n");
+	scanf("%f", &r2);
+
+	printf("Please enter a value for inner radius (r1):\n");
+	scanf("%f", &r1);
+	AnnulusMoment(r1,r2,second);
+	second = AnnulusMoment(r1,r2);
+
+	printf("The second moment of inertia is %5.2f.", second);
+	
 
 	return 0;
 }
 
 
 /* Function Defenitions */
-
+void AnnulusMoment(float num2, float num1, float *second)
+{
+	second  = .785398 * ((r2*r2*r2*r2)-(r1*r1*r1*r1));
+}
 
